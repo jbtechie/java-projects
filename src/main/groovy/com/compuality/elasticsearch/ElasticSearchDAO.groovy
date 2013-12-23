ddd8e1b158806a907fb4de56c51083cae5ff0c26
Justin Bailey <jbtechie@gmail.com>
2013-12-23 11:25:19 -0600

    add using closures with no synchronization

--- src/main/groovy/com/compuality/elasticsearch/ElasticSearchDAO.groovy
+++ src/main/groovy/com/compuality/elasticsearch/ElasticSearchDAO.groovy
@@ -1,16 +1,14 @@
 package com.compuality.elasticsearch
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.google.common.base.Throwables
 import com.google.inject.Inject
 import com.google.inject.Provider
 import org.elasticsearch.action.bulk.BulkRequestBuilder
-import org.elasticsearch.action.bulk.BulkResponse
-import org.elasticsearch.action.index.IndexRequestBuilder
 import org.elasticsearch.client.Client
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import rx.Observable
-import rx.Observer
+import rx.util.functions.Action0
+import rx.util.functions.Action1
 import rx.util.functions.Func1
 
 class ElasticSearchDAO {
@@ -28,8 +26,12 @@ class ElasticSearchDAO {
 
   void addDocuments(String index, String type, Observable<String> documents) {
     Client client = clientProvider.get()
+    BulkRequestBuilder bulkRequestBuilder = client.prepareBulk()
+
     documents.map({ client.prepareIndex(index, type).setSource(it) } as Func1)
-        .subscribe(new BulkLoadingObserver(client, index, type))
+        .doOnEach({ bulkRequestBuilder.add(it) } as Action1)
+        .doOnCompleted({ bulkRequestBuilder.execute() } as Action0)
+        .subscribe()
   }
 
   void addObjects(String index, String type, Observable<Object> objects) {
@@ -38,57 +40,15 @@ class ElasticSearchDAO {
 
   void addDocumentsWithHashId(String index, String type, Observable<String> documents) {
     Client client = clientProvider.get()
+    BulkRequestBuilder bulkRequestBuilder = client.prepareBulk()
+
     documents.map({ client.prepareIndex(index, type).setSource(it).setId(UUID.nameUUIDFromBytes(it.bytes).toString()) } as Func1)
-      .subscribe(new BulkLoadingObserver(client, index, type))
+      .doOnEach({ bulkRequestBuilder.add(it) } as Action1)
+      .doOnCompleted({ bulkRequestBuilder.execute() } as Action0)
+      .subscribe()
   }
 
   void addObjectsWithHashId(String index, String type, Observable<Object> objects) {
     addDocumentsWithHashId(index, type, objects.map({ mapper.writeValueAsString(it) } as Func1))
-  }
-
-//  private static class DocumentToIndexRequestBuilderWithHash implements Func1<String, IndexRequestBuilder> {
-//    @Override
-//    IndexRequestBuilder call(String o) {
-//      return null
-//    }
-//  }
-
-  private static class BulkLoadingObserver implements Observer<IndexRequestBuilder> {
-
-    private Client client
-    private String index
-    private String type
-    private BulkRequestBuilder builder
-
-    BulkLoadingObserver(Client client, String index, String type) {
-      logger.debug("created")
-      this.client = client
-      this.index = index
-      this.type = type
-      this.builder = client.prepareBulk()
-    }
-
-    @Override
-    synchronized void onNext(IndexRequestBuilder indexRequestBuilder) {
-      logger.debug("adding {}", indexRequestBuilder)
-      builder.add(indexRequestBuilder)
-    }
-
-    @Override
-    void onCompleted() {
-      logger.debug('executing')
-      BulkResponse response = builder.execute().actionGet()
-      if(response.hasFailures()) {
-        logger.error('FAILURES')
-        response.each {
-          logger.error("\t${it.failureMessage}")
-        }
-      }
-    }
-
-    @Override
-    void onError(Throwable e) {
-      Throwables.propagate(e)
-    }
   }
 }
