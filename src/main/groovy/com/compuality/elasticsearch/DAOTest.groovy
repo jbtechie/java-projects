c126bf62cf5e3fb449de3c5665678f15bbdb9957
Justin Bailey <jbtechie@gmail.com>
2013-12-26 21:45:42 -0600

    failed connectable

--- src/main/groovy/com/compuality/elasticsearch/DAOTest.groovy
+++ src/main/groovy/com/compuality/elasticsearch/DAOTest.groovy
@@ -1,13 +1,18 @@
 package com.compuality.elasticsearch
 
 import com.google.inject.Inject
-
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import rx.Observable
 
 class DAOTest {
 
+  private static Logger logger = LoggerFactory.getLogger(DAOTest)
+
   @Inject
   DAOTest(ElasticSearchDAO dao) {
-    dao.addObjectsWithHashId('experiments', 'exp', Observable.<Map>from([ [label:1], [label:2], [label:4]])).subscribe()
+    dao.addObjectsWithHashIdAndTime('experiments', 'exp',
+      Observable.<Map>from([ [label:1], [label:2], [label:4]]))
+        .subscribe({ logger.debug("add response: " + it.toString()) })
   }
 }
--- src/main/groovy/com/compuality/elasticsearch/ElasticSearchDAO.groovy
+++ src/main/groovy/com/compuality/elasticsearch/ElasticSearchDAO.groovy
@@ -9,6 +9,8 @@ import org.elasticsearch.client.Client
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import rx.Observable
+import rx.Subscription
+import rx.observables.ConnectableObservable
 import rx.util.functions.Func1
 
 class ElasticSearchDAO {
@@ -24,28 +26,42 @@ class ElasticSearchDAO {
     this.mapper = mapper
   }
 
-  Observable addDocuments(String index, String type, Observable<String> documents) {
+  Observable<BulkItemResponse> addDocuments(String index, String type, Observable<String> documents) {
     return bulkIndex(index, type, RANDOM_ID_FUNC, documents).subscribe()
   }
 
-  Observable addObjects(String index, String type, Observable<Object> objects) {
+  Observable<BulkItemResponse> addObjects(String index, String type, Observable<Object> objects) {
     return bulkIndex(index, type, RANDOM_ID_FUNC, objects.map({ mapper.writeValueAsString(it) }))
   }
 
-  Observable addDocumentsWithHashId(String index, String type, Observable<String> documents) {
+  Observable<BulkItemResponse> addDocumentsWithHashId(String index, String type, Observable<String> documents) {
     return bulkIndex(index, type, HASH_ID_FUNC, documents)
   }
 
-  Observable addObjectsWithHashId(String index, String type, Observable<Object> objects) {
+  Observable<BulkItemResponse> addObjectsWithHashId(String index, String type, Observable<Object> objects) {
     return bulkIndex(index, type, HASH_ID_FUNC, objects.map({ mapper.writeValueAsString(it) }))
+  }
+
+  Observable addObjectsWithHashIdAndTime(String index, String type, Observable<Object> objects) {
+    ConnectableObservable<Object> connectable = objects.synchronize().publish()
+
+//    connectable.timeInterval().subscribe({ logger.debug("Bulk time interval: " + it.getIntervalInMilliseconds()) })
+
+    Observable<BulkItemResponse> responses = bulkIndex(index, type, HASH_ID_FUNC, connectable.map({ mapper.writeValueAsString(it) }))
+    responses.subscribe()
+
+    return Observable.create({ observer ->
+      Subscription s = Observable.zip(connectable, responses, { c, r ->
+        [c, r]
+      }).subscribe(observer)
+      connectable.connect()
+      return s
+    })
   }
 
   private Observable<BulkItemResponse> bulkIndex(String index, String type, Func1<String, UUID> idFunc, Observable<String> documents) {
     Client client = clientProvider.get()
-    return documents.doOnError({ logger.error("BULK ERROR") })
-      .finallyDo({ client.close() })
-      .finallyDo({ logger.debug('Done') })
-      .buffer(1000)
+    return documents.buffer(1000)
       .mapMany({ documentBuffer ->
         BulkRequestBuilder bulkBuilder = client.prepareBulk()
         documentBuffer.each {
@@ -56,6 +72,9 @@ class ElasticSearchDAO {
         BulkResponse bulkResponse = bulkBuilder.execute().actionGet()
         return Observable.from(bulkResponse.getItems())
       })
+      .doOnError({ logger.error("Error performing bulk index.") })
+      .finallyDo({ client.close() })
+      .finallyDo({ logger.debug("Done performing bulk index.") })
   }
 
   private static final RANDOM_ID_FUNC = { UUID.randomUUID() }
