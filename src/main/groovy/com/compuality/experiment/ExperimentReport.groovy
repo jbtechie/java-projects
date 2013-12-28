854ec53deae776d4e04bacaf3ccd6a891264a024
Justin Bailey <jbtechie@gmail.com>
2013-12-28 17:06:30 -0600

    tmp

--- src/main/groovy/com/compuality/experiment/ExperimentReport.groovy
+++ src/main/groovy/com/compuality/experiment/ExperimentReport.groovy
@@ -1,7 +1,6 @@
 package com.compuality.experiment
-import com.google.common.collect.ImmutableMap
+
 import rx.Observable
-import rx.Observer
 
 class ExperimentReport {
 
@@ -9,18 +8,11 @@ class ExperimentReport {
   Map<String, Object> args
   Observable<GenerationReport> generations
 
-  static class CreateRequest extends ExperimentReport {}
-
   static class GenerationReport {
 
     UUID experimentId
     long index
-
-    static class CreateRequest extends GenerationReport {}
-
-    static class Builder {
-
-    }
+    Observable<LifeReport> lives
   }
 
   static class LifeReport {
@@ -28,8 +20,6 @@ class ExperimentReport {
     UUID experimentId
     long generationIndex
     double error
-
-    static class CreateRequest extends LifeReport {}
   }
 
   static interface DAO {
@@ -38,93 +28,6 @@ class ExperimentReport {
 
     ExperimentReport get(UUID id)
 
-    Observable<GenerationReport> getGenerationReports(UUID experimentId)
-
-    Observable<LifeReport> getLifeReports(UUID experimentId, long generationIndex)
-
-    Observable<ExperimentReport> createExperiments(Observable<CreateRequest> request)
-
-    Observable<GenerationReport> createGenerations(Observable<GenerationReport.CreateRequest> request)
-
-    Observable<LifeReport> createLives(Observable<LifeReport.CreateRequest> request)
-  }
-
-  static interface Loader {
-
     void loadExperiments(Observable<ExperimentReport> reports)
-    void loadGenerations(Observable<GenerationReport> reports)
-    void loadLives(Observable<LifeReport> reports)
-  }
-
-  static class LifeReportObserver implements Observer<LifeReport> {
-    void onCompleted() {
-
-    }
-
-    void onError(Throwable e) {
-
-    }
-
-    void onNext(LifeReport args) {
-
-    }
-  }
-
-  public static Observable<ExperimentReport> experiment(Observable<Map<String, Object>> argsSource) {
-    return argsSource.map({ args ->
-      //...do experiment...
-
-      return new ExperimentReport([id:UUID.randomUUID(), args:args, generations:generations(id)])
-    })
-  }
-
-  public static Observable<GenerationReport> generations(UUID id) {
-    return Observable.create({ observer ->
-      Random rand = new Random()
-      long index = 0
-      def prevGen = null
-      while(rand.nextInt() != 0) {
-        prevGen = gen(prevGen)
-        def genReport = prevGen.report()
-        genReport.lives.doOnCompleted({ })
-        observer.onNext(genReport)
-//        observer.onNext(GenerationReport([id:id, index:index, lives:lives(id, index)]))
-      }
-    })
-  }
-
-  def gen(def prevGen) {
-
-  }
-
-  public static Observable<LifeReport> lives(UUID id, long index) {
-
-  }
-
-  def foo() {
-    experiment(Observable.from([arg1:"val1"])).subscribe({ ExperimentReport experiment ->
-      dao.addExperiment(experiment)
-      experiment.generations.subscribe({ GenerationReport generation ->
-        dao.addGeneration(generation)
-      })
-    })
-  }
-
-  static class Builder {
-
-    final UUID id
-    final Map<String, Object> args
-    final List<GenerationReport.Builder> genBuilders
-
-    Builder(UUID id, Map<String, Object> args) {
-      this.id = id
-      this.args = ImmutableMap.copyOf(args)
-    }
-
-    GenerationReport.Builder addGeneration() {
-      def builder = new GenerationReport.Builder()
-      genBuilders.add(builder)
-      return builder
-    }
   }
 }
--- src/main/groovy/com/compuality/experiment/ExperimentRunner.groovy
+++ src/main/groovy/com/compuality/experiment/ExperimentRunner.groovy
@@ -1,97 +1,39 @@
 package com.compuality.experiment
+
 import com.compuality.cpu.CPU
 import com.compuality.experiment.ExperimentReport.GenerationReport
 import com.compuality.experiment.ExperimentReport.LifeReport
-import com.compuality.experiment.ExperimentReport.Loader
 import rx.Observable
 
 class ExperimentRunner {
 
-  void run(Loader loader) {
-    Observable<ExperimentReport> experiments = Observable.create({ experimentObserver ->
-      (1..10).each {
-        Observable<GenerationReport> generations = Observable.create({ generationObserver ->
-          (1..1000).each {
-            GenerationReport generationReport = new GenerationReport()
-
-            Observable<LifeReport> lives = Observable.create({ lifeObserver ->
-              (1..100).each {
-                CPU cpu = new CPU(6)
-                cpu.randMem()
-                cpu.sim(cpu.WORD_SIZE)
-                lifeObserver.onNext(new LifeReport([experimentId:generationReport.experimentId, generationIndex:generationReport.index]))
-              }
-              lifeObserver.onCompleted()
-            })
-
-            loader.loadLives(lives)
-
-            generationObserver.onNext(generationReport)
-          }
-          generationObserver.onCompleted()
-        })
-
-        loader.loadGenerations(generations)
-
-        experimentObserver.onNext()
-      }
-
-      experimentObserver.onCompleted()
-    })
-
-    loader.loadExperiments(experiments)
-  }
-
-  Observable<ExperimentReport> prepareExperiments() {
-    return Observable.create({ experimentObserver ->
-      (1..10).each {
-        Observable<GenerationReport> generations = Observable.create({ generationObserver ->
-          (1..1000).each {
-            GenerationReport generationReport = new GenerationReport()
-
-            Observable<LifeReport> lives = Observable.create({ lifeObserver ->
-              (1..100).each {
-                CPU cpu = new CPU(6)
-                cpu.randMem()
-                cpu.sim(cpu.WORD_SIZE)
-                lifeObserver.onNext(new LifeReport([experimentId:generationReport.experimentId, generationIndex:generationReport.index]))
-              }
-              lifeObserver.onCompleted()
-            })
-
-            generationObserver.onNext(generationReport)
-          }
-          generationObserver.onCompleted()
-        })
-
-        experimentObserver.onNext()
-      }
-
-      experimentObserver.onCompleted()
-    })
-  }
-
-  Observable<ExperimentReport> experiments(Observable<Map<String, Object>> argsSource) {
+  static Observable<ExperimentReport> experiments(Observable<Map<String, Object>> argsSource) {
     return argsSource.map({ args ->
-      UUID experimentId = UUID.randomUUID()
-      Observable<GenerationReport> generations = Observable.create({ generationObserver ->
-        (1..1000).each { genIndex ->
-          Observable<LifeReport> lives = Observable.create({ lifeObserver ->
-            (1..100).each {
-              CPU cpu = new CPU(6)
-              cpu.randMem()
-              cpu.sim(cpu.WORD_SIZE)
-              lifeObserver.onNext(new LifeReport([experimentId:experimentId, generationIndex:genIndex]))
-            }
-            lifeObserver.onCompleted()
-          })
+      UUID id = UUID.randomUUID()
+      return new ExperimentReport([id:id, args:args, generations:generations(id)])
+    })
+  }
 
-          generationObserver.onNext(new GenerationReport([experimentId:experimentId, index:genIndex, lives:lives]))
-        }
-        generationObserver.onCompleted()
-      })
+  private static Observable<GenerationReport> generations(UUID experimentId) {
+    return Observable.create({ generationObserver ->
+      (1..1000).each { genIndex ->
+        Observable<LifeReport> lives = lives(experimentId, genIndex)
+        GenerationReport generation = new GenerationReport([experimentId:experimentId, index:genIndex, lives:lives])
+        generationObserver.onNext()
+      }
+      generationObserver.onCompleted()
+    })
+  }
 
-      return new ExperimentReport([id:experimentId, args:args, generations:generations])
+  private static Observable<LifeReport> lives(UUID experimentId, long generationIndex) {
+    return Observable.create({ lifeObserver ->
+      (1..100).each {
+        CPU cpu = new CPU(6)
+        cpu.randMem()
+        cpu.sim(cpu.WORD_SIZE)
+        lifeObserver.onNext(new LifeReport([experimentId:experimentId, generationIndex:generationIndex]))
+      }
+      lifeObserver.onCompleted()
     })
   }
 }
